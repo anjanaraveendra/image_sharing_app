d6d87a40e7891f6874cbae6a9c96eb4ee6ac458b
Anjana <anjana@Anjanas-MacBook-Pro.local>
2015-10-08 14:30:36 +0530

    Some changes :a:

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,11 @@
 module HomeHelper
+
+	def username id
+		u = User.find(id)
+		if u.first_name.present? && u.last_name.present?
+			name = u.first_name + " " +u.last_name
+			return name
+		end
+	end
+
 end
