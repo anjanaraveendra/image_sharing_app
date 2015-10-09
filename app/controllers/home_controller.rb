52483b0c02d732a2dbc503cb45cddd5157aa0ddf
Anjana <anjana@Anjanas-MacBook-Pro.local>
2015-10-09 13:14:09 +0530

    My Galleries added :a:

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,9 @@ class HomeController < ApplicationController
   def index
   	@galleries = Gallery.all.limit(25)
   end
+
+  def user_gallery
+  	@galleries = Gallery.where('user_id = ?',current_user.id)
+  end
+
 end
